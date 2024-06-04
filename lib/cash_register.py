d7f0d9f5cf04edae0db5cf2876dcc8dbcefb886e
jammie-commits <jay.mbugua.ph@gmail.com>
2024-06-05 01:10:42 +0300

    9 passing

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.total = 0.0
+        self.items = []
+        self.discount = discount
+
+    def add_item(self, item_name, price, quantity=1):
+        for _ in range(quantity):
+            self.items.append((item_name, price))
+            self.total += price
+
+    def apply_discount(self):
+        if self.discount > 0:
+            self.total -= self.total * (self.discount / 100)
+            print(f"Discount applied. New total: ${self.total:.2f}")
+        else:
+            print("There is no discount to apply.")
+
+    def void_last_transaction(self):
+        if self.items:
+            last_item = self.items.pop()
+            self.total -= last_item[1]
+
+
